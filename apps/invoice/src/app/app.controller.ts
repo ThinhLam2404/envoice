e9aca8f2868a5c244b3b764697b1a8a7a3742646
thinh <thinhlam240499@gmail.com>
2025-12-24 21:32:09 +0700

    build(docker mongo): setup docker and mongo configuration

--- apps/invoice/src/app/app.controller.ts
+++ apps/invoice/src/app/app.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
-
+import { TcpLoggingInterceptor } from '@common/interceptors/tcpLogging.interceptor';
+import { Response } from '@common/interfaces/tcp/common/response.interface';
+import { Request } from '@common/interfaces/tcp/common/request.interface';
+import { ProcessId } from '@common/decorators/processId.decorator';
+import { RequestParams } from '@common/decorators/request-param.decorator';
 @Controller()
+@UseInterceptors(TcpLoggingInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -12,7 +17,15 @@ export class AppController {
   }
 
   @MessagePattern('get_invoice')
-  getInvoice(id: number): string {
-    return `Invoice #${id} details from Invoice Service`;
+  getInvoice(
+    @ProcessId() processId: string,
+    @RequestParams() params: { invoiceId: string; invoiceName: string },
+    @RequestParams('invoiceId') invoiceId: string,
+  ): Response<string> {
+    Logger.debug(`Processing invoice with ID: ${invoiceId}`);
+    Logger.debug(`Process ID: ${processId}`);
+    Logger.debug(`Params: ${JSON.stringify(params)}`);
+
+    return Response.success<string>(`Invoice: ${invoiceId}`);
   }
 }
