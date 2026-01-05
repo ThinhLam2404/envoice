6f22e6eaf0d34f3a87cc6b373618a2e898730bb5
thinh <thinhlam240499@gmail.com>
2026-01-05 15:29:24 +0700

    feat(pdf generator): add pdf generator service

---
+++ apps/pdf-generator/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CONFIGURATION, type TConfiguration } from '../configuration';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  static CONFIGURATION: TConfiguration = CONFIGURATION;
+}
